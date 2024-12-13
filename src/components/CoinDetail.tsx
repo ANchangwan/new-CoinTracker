f2c020c29833f0e15b330ddd96e28cc39e8999db
waney <ssdii66@gmail.com>
2024-12-14 04:23:19 +0900

    PriceTrendindicator component 수정 -> 하락 감속 에 따라 색 표현(하락 빨강 증가 초록 )

--- src/components/CoinDetail.tsx
+++ src/components/CoinDetail.tsx
@@ -5,7 +5,7 @@ import PriceTrendIndicator from "./PriceTrendIndicator.tsx";
 
 
 function CoinDetail({coin}:{coin:IinfoData}) {
-    const {coinId} = useParams()
+    const {coinId} = useParams();
 
     return (
         <div className="bg-amber-900 text-white p-12 mr-4">
@@ -16,7 +16,7 @@ function CoinDetail({coin}:{coin:IinfoData}) {
             <div className="flex items-center gap-4 mb-2">
                 <img src={coin.image.small} alt=""/>
                 <div className="uppercase font-semibold">{coinId}</div>
-                <div className="text-xl text-white w-20 h-10 bg-green-400 flex justify-center items-center rounded-xl">
+                <div className="text-xl text-white w-20 h-10 flex justify-center items-center rounded-xl">
                     <PriceTrendIndicator data={coin}  />
                 </div>
                 <div className="text-xl text-white font-semibold ">{coin.tickers[0].last}</div>
--- src/components/PriceTrendIndicator.tsx
+++ src/components/PriceTrendIndicator.tsx
@@ -5,16 +5,14 @@ import {IinfoData} from "../types/type.ts";
 function PriceTrendIndicator({data}: { data: IinfoData }) {
 
     return (
-        <span className={`${
-            parseFloat(String(data.market_data.price_change_percentage_24h)) < 0
-                ? 'text-red-500'
-                : parseFloat(String(data.market_data.price_change_percentage_24h)) > 0
-                    ? 'text-white-500'
-                    : 'text-gray-500'
-        }`}>
-            {String(data.market_data.price_change_percentage_24h).slice(0, 4)}%
-        </span>
+        <div className={`${parseFloat(String(data.market_data.price_change_percentage_24h)) > 0 ? "bg-green-400":"bg-red-500"} 
+            text-xl text-white w-20 h-10 flex justify-center items-center rounded-xl`}>
+            <span>
+                {String(data.market_data.price_change_percentage_24h).slice(0, 4)}%
+            </span>
+        </div>
     )
 }
 
 export default PriceTrendIndicator;
+
